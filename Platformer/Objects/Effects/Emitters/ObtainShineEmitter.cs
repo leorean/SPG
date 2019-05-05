1807f65abee10ae5f9a871c3d7cd8dd885901da6
Peter S <peter@anyline.io>
2019-05-05 12:24:11 +0200

    added missing files

---
+++ Platformer/Objects/Effects/Emitters/ObtainShineEmitter.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Platformer.Objects.Main;
+using SPG.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformer.Objects.Effects.Emitters
+{
+    public class ObtainShineParticle : Particle
+    {
+        float s = .01f;
+        public float lightScale;
+
+        public ObtainShineParticle(ParticleEmitter emitter) : base(emitter)
+        {
+            DrawOffset = new Microsoft.Xna.Framework.Vector2(32, 32);
+            Texture = AssetManager.WhiteCircleSprite;
+
+            LifeTime = 600;
+
+            Scale = new Vector2(s);            
+            Depth = emitter.Depth;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            s = (float)Math.Min(s + .02 * lightScale, lightScale * 5f);
+
+            Scale = new Vector2(s);
+
+            Alpha = Math.Max(Alpha - .001f, 0);
+            Position = Emitter.Position;
+        }
+    }
+
+    public class ObtainShineEmitter : ParticleEmitter
+    {
+        public float GlowScale;
+        public float GlowAlpha;
+
+        public ObtainShineEmitter(float x, float y) : base(x, y)
+        {
+            SpawnTimeout = 15;
+            //GlowAlpha = .5f;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void CreateParticle()
+        {
+            var particle = new ObtainShineParticle(this);
+            particle.lightScale = GlowScale;
+            particle.Alpha = GlowAlpha;
+        }
+    }
+}
---
+++ Platformer/Objects/Main/MessageBox.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Platformer.Main;
+using SPG.Draw;
+using SPG.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformer.Objects.Main
+{
+    public class MessageBox : GameObject
+    {
+        string text;
+        Font font;
+        int maxWidth;
+
+        int offY = 5 * Globals.TILE;
+
+        public MessageBox(float x, float y, string text, string name = null) : base(x, y, name)
+        {
+            this.text = text;
+            Depth = Globals.LAYER_FONT - .001f;
+
+            Texture = AssetManager.MessageBoxSprite;
+
+            font = AssetManager.DefaultFont.Copy();
+            font.Halign = Font.HorizontalAlignment.Left;
+            font.Valign = Font.VerticalAlignment.Top;
+
+            maxWidth = RoomCamera.Current.ViewWidth - Globals.TILE - 4;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            Position = new Vector2(RoomCamera.Current.ViewX, RoomCamera.Current.ViewY + offY);
+            
+        }
+
+        public override void Draw(SpriteBatch sb, GameTime gameTime)
+        {
+            base.Draw(sb, gameTime);
+
+            font.Draw(sb, X + 8 + 2, Y + 8, text, maxWidth);
+        }
+    }
+}
